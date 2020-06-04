5fbbecdc878b29791b9b84f31056d7c0de403790
Kamran <kamran.nazir047@gmail.com>
2020-06-04 16:04:18 +0500

    2nd Commit By Kamran

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React (By Kamran)
+          Learn React (By Kamran) 2nd Commit
         </a>
       </header>
     </div>
