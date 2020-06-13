63ea36df2d573f1b5db5e6142de7fd61b5896f45
Usama Majid <usamamajid6@gmail.com>
2020-06-13 13:37:43 +0500

    Commit By Usama

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ class App extends Component {
         <Col span={24}>
           <Modal width="70vw" centered={true} visible={true}>
             <UCP />
+            <div>
+              Usama
+            </div>
           </Modal>
         </Col>
       </Row>
